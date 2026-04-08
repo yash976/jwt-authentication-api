fee7f74b1bce1186bd8683e79de8f2ccaa33a9a4
Yash Advani <54671322+yash976@users.noreply.github.com>
2026-04-08 11:39:49 +0530

    Create JwtAuthenticationApiApplication.java
    Add main Spring Boot application class

---
+++ src/main/java/com/jwtauth/api/JwtAuthenticationApiApplication.java
@@ -0,0 +1,12 @@
+package com.jwtauth.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthenticationApiApplication.class, args);
+    }
+}
