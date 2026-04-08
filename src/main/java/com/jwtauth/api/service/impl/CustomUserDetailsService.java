58a382201cfb6ded3c8c2bc582c9038fd7f3e740
Yash Advani <54671322+yash976@users.noreply.github.com>
2026-04-08 12:15:24 +0530

    Create CustomUserDetailsService.java
    Add CustomUserDetailsService for loading users with Spring Security

---
+++ src/main/java/com/jwtauth/api/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.jwtauth.api.service.impl;
+
+import com.jwtauth.api.model.User;
+import com.jwtauth.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities(user.getRole().name())
+                .build();
+    }
+}
