37ffa7f036e17d1893464d0380529b2424f8b9e2
Yash Advani <54671322+yash976@users.noreply.github.com>
2026-04-08 12:12:23 +0530

    Create User.java
    Add User model with JPA mapping

---
+++ src/main/java/com/jwtauth/api/model/User.java
@@ -0,0 +1,26 @@
+package com.jwtauth.api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
